6415f672b4d5f31bfda97ae5a6643b0847b66757
Utkarsh Vishnoi <utkarsh.v8274@gmail.com>
2025-08-29 11:27:29 +0530

    Added the handling of invalid endpoints

--- server.js
+++ server.js
@@ -70,5 +70,9 @@ app.post("/bfhl", (req, res) => {
   }
 });
 
+app.use((req, res) => {
+  res.status(404).send("API endpoint not found");
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
